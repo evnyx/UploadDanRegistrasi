cc81b3fbffe4860dff94077cf0ab72700017a30b
Ikhlaashul Mu'aasyiqiin <39987415+evnyx@users.noreply.github.com>
2018-11-19 10:43:37 +0700

    Add files via upload

---
+++ tambah_data.php
@@ -0,0 +1,31 @@
+<?php
+    require 'tambah.php';
+
+
+    if(isset($_POST['submit']))
+    {
+        var_dump($_POST);
+        var_dump($_FILES);
+        die();
+
+
+        if(tambah($_POST)>0)
+        {
+           echo "
+            <script>
+                alert('data berhasil disimpan');
+                document.location.href='index.php';
+            </script>
+
+            ";
+        }else{
+            echo "
+            <script>
+                alert('data gagal disimpan');
+                document.location.href='tambah_data.php';
+            </script>";
+            echo "<br>";
+            echo mysqli_error($conn);
+        }
+    }
+?>
